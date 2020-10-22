cd583ae88a935b5538d7e7459762bc371cf40069
p-MAX-q <pmaxq@users.noreply.github.com>
2020-10-23 01:06:02 +0500

    Добавьте файлы проекта.

---
+++ calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] array1 = { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[] array2 = { 2, 3, 4 };
+
+            string text1 = "";
+            foreach (int i in array1)
+            {
+                text1 += i.ToString();
+            }
+            Console.WriteLine(text1);
+
+            string text2 = "";
+            foreach (int i in array2)
+            {
+                text2 += i.ToString();
+            }
+            Console.WriteLine(text2);
+
+            Console.WriteLine(text1.Contains(text2));
+        }
+    }
+}
