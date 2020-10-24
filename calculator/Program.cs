8ead48f306fc6c19554592bf56b2e80c5034001a
p-MAX-q <pmaxq@users.noreply.github.com>
2020-10-24 09:03:37 +0500

    Update Program.cs

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -7,7 +7,6 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-
             int[,] array1 = { { 1, 2, 3 }, { 4, 5, 6 } };
             int[] array2 = { 1, 2, 3 };
 
