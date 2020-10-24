5c0a9446fd7986109bf0f764509d127cb24f0ace
p-MAX-q <pmaxq@users.noreply.github.com>
2020-10-24 10:31:14 +0500

    Update Program.cs

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -9,12 +9,14 @@ namespace calculator
         static void Main(string[] args)
         {
             int[,] array1 = { { 1, 2, 3 }, { 4, 5, 6 } };
-            int[] array2 = { 4, 5, 6 };
+            int[] array2 = { 3, 4, 5 };
 
-            string text1 = ArrayToString(array1);
-            string text2 = ArrayToString(array2);
+            string text1 = Distribution(array1);
+            string text2 = Distribution(array2);
 
             Console.WriteLine(text1.Contains(text2));
+
+            Console.WriteLine(Check(array1));
         }
 
         static string ArrayToString(Array x)
@@ -23,6 +25,34 @@ namespace calculator
             foreach (int i in x)
                 text += i.ToString();
             return text;
+        }
+
+        static string Check(Array x)
+        {
+            string str = "";
+            int k = 0;
+            foreach (int i in x)
+            {
+                k++;
+                if (k < x.Length / x.Rank)
+                    str += i.ToString();
+                else
+                {
+                    k = 0;
+                    str += i.ToString() + " ";
+                }
+            }
+            return str;
+        }
+
+        static string Distribution(Array x)
+        {
+            string str = "";
+            if (x.Rank == 1)
+                str = ArrayToString(x);
+            else
+                str = Check(x);
+            return str;
         }
     }
 }
