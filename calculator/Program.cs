83fb974758756795d0acf0887285ba2452eed1d8
p-MAX-q <pmaxq@users.noreply.github.com>
2020-10-24 08:39:45 +0500

    Update Program.cs

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -9,23 +9,20 @@ namespace calculator
         {
 
             int[,] array1 = { { 1, 2, 3 }, { 4, 5, 6 } };
-            int[] array2 = { 2, 3, 4 };
+            int[] array2 = { 1, 2, 3 };
 
-            string text1 = "";
-            foreach (int i in array1)
-            {
-                text1 += i.ToString();
-            }
-            Console.WriteLine(text1);
-
-            string text2 = "";
-            foreach (int i in array2)
-            {
-                text2 += i.ToString();
-            }
-            Console.WriteLine(text2);
+            string text1 = ArrayToString(array1);
+            string text2 = ArrayToString(array2);
 
             Console.WriteLine(text1.Contains(text2));
+        }
+
+        static string ArrayToString(Array x)
+        {
+            string text = "";
+            foreach (int i in x)
+                text += i.ToString();
+            return text;
         }
     }
 }
